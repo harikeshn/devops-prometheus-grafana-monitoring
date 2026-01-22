a5ba2d23a4f6656fe3cd40d06e84ec5ecb4fa3e6
Harikesh N <harikeshn0003@gmail.com>
2026-01-22 12:18:15 +0530

    Create install-grafana.sh

---
+++ script/install-grafana.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https software-properties-common
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+
+sudo apt-get update
+sudo apt-get install -y grafana
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
+
+echo "Grafana installed"
