6e0c6393d7583281ed5515179e1a6086fe0683cc
Harikesh N <harikeshn0003@gmail.com>
2026-01-22 12:17:46 +0530

    Create install-prometheus.sh

---
+++ script/install-prometheus.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo useradd --no-create-home prometheus
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+
+cd /tmp
+wget https://github.com/prometheus/prometheus/releases/download/v2.49.0/prometheus-2.49.0.linux-amd64.tar.gz
+tar xvf prometheus-2.49.0.linux-amd64.tar.gz
+
+sudo cp prometheus-2.49.0.linux-amd64/prometheus /usr/local/bin/
+sudo cp prometheus-2.49.0.linux-amd64/promtool /usr/local/bin/
+
+echo "Prometheus installed"
